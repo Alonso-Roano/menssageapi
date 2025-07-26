cd6c449935d9dcb127cf547e0ffecf1d8b79b140
Angel <angelguzman10911@gmail.com>
2025-07-25 19:18:34 -0500

    update cors

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import mensajes from './src/mensajes';
 import auth from './src/auth';
-
+import cors from 'cors';
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use('/api', auth);
 app.use('/api', mensajes);
 
